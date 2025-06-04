8951695d51a79645f132bbcb330c74c38b64e4a2
mayarei2662 <mayarei2662@gmail.com>
2025-06-04 18:03:03 +0900

    6秒の時にボタンを押すとゲームクリア！と表示、完成！

--- Assets/timemanager.cs
+++ Assets/timemanager.cs
@@ -4,37 +4,35 @@ using UnityEngine.UI;
 
 public class Timemanagers : MonoBehaviour
 {
-   
+
     //制限時間
     [SerializeField] int timeLimit;
     //タイマーテキストの操作用
-    //他のところで使う場合、パブリックにしておく
-    [SerializeField]public Text timerText;
+    [SerializeField] public Text timerText;
     //経過時間
     float timerTime;
-    //残り時間判定用（他のところで使わないなら書かなくて良い）
-    // 残り時間（静的フィールドで共有可能）
+    //残り時間判定用
     public static int remaining;
-    // Start is called before the first frame update
+    public bool isWorking = true;   // 動いているか
+  
     void Start()
-    { 
-        
+    {
+
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        if (!isWorking)
+        {
+            //空の結果を返す
+            return;
+        }
+
         //フレームごとの経過時間をtime変数に追加
-        //経過時間が1秒、2秒…
         timerTime += Time.deltaTime;
         //timertimeをint型にし制限時間から引き、代入(残り時間)
-        //他のところから使いたい場合、直接指定した方が良い。
         Timemanagers.remaining = timeLimit - (int)timerTime;
-        //timerTextを更新！
-        //$を使うと文字列内に変数や式を埋め込むことができる
-        //remaining.ToString("D3"):
-        //この部分は、remainingという整数値を3桁に整形し、足りない桁を0で埋める働きをする。
         timerText.text = $"のこり:{remaining.ToString("D3")}";
 
         //0以下の時はずっとTimeUP!と表示し続ける
@@ -44,5 +42,5 @@ public class Timemanagers : MonoBehaviour
         }
 
     }
-   
+
 }
--- Assets/timesix.cs
+++ Assets/timesix.cs
@@ -3,13 +3,13 @@ using UnityEngine;
 public class timesix : MonoBehaviour
 {
     private bool isClicked = false;
-    // Start is called before the first frame update
+
     void Start()
     {
 
     }
 
-    // Update is called once per frame
+
 
 
 
@@ -20,13 +20,14 @@ public class timesix : MonoBehaviour
         {
             Debug.Log("ゲームクリア！");
             //textのようなUI系はSelializeFieldが一番良い
-            //この下を修正しないとダメかも…
-            //6秒になってボタンを押したら画面にゲームクリアと表示したい…
+
+
             Timemanagers timerManager = FindObjectOfType<Timemanagers>();
 
             if (timerManager != null)
             {
-                timerManager.timerText.text = "ゲームクリア！"; // UIに表示
+                timerManager.timerText.text = "ゲームクリア！";
+                timerManager.isWorking = false;
             }
 
             isClicked = false; // フラグをリセットして再実行を防止
